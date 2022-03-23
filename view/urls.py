aabe0abdd65636b4de7311beb52988e44975d4d2
Linlin Yan <yanlinlin82@gmail.com>
2022-03-23 23:54:33 +0800

    Show single paper and list by label.

--- view/urls.py
+++ view/urls.py
@@ -5,4 +5,6 @@ from . import views
 #app_name = 'view'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('paper/<int:id>', views.paper, name='paper'),
+    path('label/<str:text>', views.label, name='label'),
 ]
--- view/views.py
+++ view/views.py
@@ -8,12 +8,26 @@ from .models import Label, Paper
 
 # Create your views here.
 def index(request):
+    paper_list = Paper.objects.order_by('-create_time')
+    template = loader.get_template('index.html')
+    context = {
+        'paper_list': paper_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def paper(request, id):
+    paper = Paper.objects.filter(pk=id)
+    template = loader.get_template('single.html')
+    context = {
+        'paper': paper[0],
+    }
+    return HttpResponse(template.render(context, request))
+
+def label(request, text):
     paper_list = None
-    xiangma = Label.objects.filter(name="xiangma")
+    xiangma = Label.objects.filter(name=text)
     if xiangma.count() > 0:
-        paper_list = xiangma[0].paper_set.all()
-    #paper_list = Paper.objects.filter(Q(labels="xiangma")).order_by('-create_time')
-    #paper_list = Paper.objects.order_by('-create_time')
+        paper_list = xiangma[0].paper_set.all().order_by('-create_time')
     template = loader.get_template('index.html')
     context = {
         'paper_list': paper_list,
