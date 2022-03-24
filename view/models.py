08ea4abf8acbe99438b8b79f7c7d885176340f64
Linlin Yan <yanlinlin82@gmail.com>
2022-03-24 10:04:49 +0800

    Add more paper fields.

--- view/models.py
+++ view/models.py
@@ -9,17 +9,22 @@ class Label(models.Model):
         return self.name
 
 class Paper(models.Model):
-    creator = models.CharField(max_length=100)
+    creator = models.CharField(max_length=100, default='')
     creator_weixin_id = models.CharField(max_length=100)
     create_time = models.DateTimeField('date published')
-    doi = models.CharField(max_length=100)
-    pmid = models.CharField(max_length=20)
-    pmcid = models.CharField(max_length=100)
-    arxiv_id = models.CharField(max_length=100)
-    journal = models.CharField(max_length=200)
-    publish_year = models.CharField(max_length=10)
-    title = models.CharField(max_length=500)
-    comments = models.CharField(max_length=65536)
+    doi = models.CharField(max_length=100, default='')
+    pmid = models.CharField(max_length=20, default='')
+    pmcid = models.CharField(max_length=100, default='')
+    arxiv_id = models.CharField(max_length=100, default='')
+    journal = models.CharField(max_length=200, default='')
+    publish_year = models.CharField(max_length=10, default='')
+    title = models.CharField(max_length=500, default='')
+    comments = models.CharField(max_length=65536, default='')
     labels = models.ManyToManyField(Label)
+    authors = models.CharField(max_length=65536, default='') # json format
+    abstract = models.CharField(max_length=65536, default='')
+    urls = models.CharField(max_length=65536, default='') # text of lines
+    is_private = models.BooleanField(default=True)
+    full_text = models.FileField(default='')
     def __str__(self):
         return self.creator + ' - ' + self.publish_year + ' - ' + self.journal + ' - ' + self.title
