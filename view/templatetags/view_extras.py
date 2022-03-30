2a8515a681cd1e9ca559afd421fde1e0ecb78021
Linlin Yan <yanlinlin82@gmail.com>
2022-03-30 11:57:20 +0800

    Update view.

--- view/templatetags/view_extras.py
+++ view/templatetags/view_extras.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter(name='replace_line_br')
 def replace_line_br(value):
-    return value.replace("\n", "<br><br>")
+    return value.replace("\n", "<br>")
 
 @register.filter(name='is_for_xiangma')
 def is_for_xiangma(value):
