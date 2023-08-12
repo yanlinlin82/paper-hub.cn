ac70e1410c78657d13c2afa7ec0b4deb33d4301a
Linlin Yan <yanlinlin82@gmail.com>
2023-08-12 15:02:35 +0800

    Add models to admin.

--- view/admin.py
+++ view/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Paper
+from .models import User, Label, Paper, CrossRefCache, Collection
 
+admin.site.register(User)
+admin.site.register(Label)
 admin.site.register(Paper)
+admin.site.register(CrossRefCache)
+admin.site.register(Collection)
