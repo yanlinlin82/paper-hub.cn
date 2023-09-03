4d7da093d6bfc033384c17214cde4445d95a2fa2
Linlin Yan <yanlinlin82@gmail.com>
2023-09-03 08:49:35 +0800

    Remove CrossRefCache, use file cache instead of db cache.

--- view/admin.py
+++ view/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Label, Paper, CrossRefCache, Collection
+from .models import User, Label, Paper, Collection
 
 admin.site.register(User)
 admin.site.register(Label)
 admin.site.register(Paper)
-admin.site.register(CrossRefCache)
 admin.site.register(Collection)
