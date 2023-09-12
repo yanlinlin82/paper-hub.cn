58273ea1ecf9c118cc1e6421c0778a4b175f08d1
Linlin Yan <yanlinlin82@gmail.com>
2023-09-12 20:14:22 +0800

    Fix bug in paginator.

--- group/templatetags/paginator.py
+++ group/templatetags/paginator.py
@@ -4,6 +4,6 @@ from django.core.paginator import Paginator
 register = template.Library()
 
 @register.simple_tag
-def get_elided_page_range(p, number):
-    paginator = Paginator(p.object_list, p.per_page)
-    return paginator.get_elided_page_range(number=number)
+def get_elided_page_range(p, page_number, on_each_side=3, on_ends=2):
+    return p.paginator.get_elided_page_range(number=page_number,
+                                             on_each_side=on_each_side, on_ends=on_ends)
