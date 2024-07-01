54924d447f2e3755d73be395815eac2b270c5acc
Linlin Yan <linlin.yan@bioinfo.app>
2024-07-01 18:45:48 +0800

    Add table/fields for translation.

--- view/models.py
+++ view/models.py
@@ -87,6 +87,11 @@ class Paper(models.Model):
     def __str__(self):
         return f'{self.pub_year}, {self.journal}, {self.title}'
 
+class PaperTranslation(models.Model):
+    paper = models.ForeignKey(Paper, on_delete=models.CASCADE, related_name='translations')
+    title_cn = models.CharField(max_length=4096, default='', blank=True)
+    abstract_cn = models.CharField(max_length=65536, default='', blank=True)
+
 class Label(models.Model): # every user has his own labels
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=128, default='')
