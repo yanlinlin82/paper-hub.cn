540140f6b266c08d4d9bc48f343a238112cb5362
Linlin Yan <linlin.yan@bioinfo.app>
2024-07-05 02:04:07 +0800

    Improve Paper model.

--- view/models.py
+++ view/models.py
@@ -68,10 +68,8 @@ class UserSession(models.Model):
         return f"{self.user.nickname}: {self.token} - {self.get_client_type_display()}"
 
 class Paper(models.Model):
-    create_time = models.DateTimeField(default=timezone.now)
-    update_time = models.DateTimeField(default=timezone.now)
-    #create_time = models.DateTimeField(auto_now_add=True, db_index=True)
-    #update_time = models.DateTimeField(auto_now=True, db_index=True)
+    create_time = models.DateTimeField(auto_now_add=True, db_index=True)
+    update_time = models.DateTimeField(auto_now=True, db_index=True)
 
     journal = models.CharField(max_length=256, default='', blank=True)
     pub_date = models.CharField(max_length=64, default='', blank=True)
@@ -91,8 +89,8 @@ class Paper(models.Model):
 
     language = models.CharField(max_length=20, default='eng') # eng, chi, etc.
 
-    #class Meta:
-    #    ordering = ['-create_time']
+    class Meta:
+        ordering = ['-create_time']
 
     def __str__(self):
         return f'{self.pub_year}, {self.journal}, {self.title}'
