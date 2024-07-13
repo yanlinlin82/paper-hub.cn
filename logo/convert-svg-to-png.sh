c0d35c093660ad35f309cd11892e284694ae109a
Linlin Yan <linlin.yan@bioinfo.app>
2024-07-14 01:49:10 +0800

    Add website logo.

---
+++ logo/convert-svg-to-png.sh
@@ -0,0 +1,4 @@
+rsvg-convert -a paper-hub-logo.40x40.svg -o paper-hub-logo.40x40.png
+
+rsvg-convert -a paper-hub-banner.40x150-white-text.svg -o paper-hub-banner.40x150-white-text.png
+rsvg-convert -a paper-hub-banner.40x150-black-text.svg -o paper-hub-banner.40x150-black-text.png
