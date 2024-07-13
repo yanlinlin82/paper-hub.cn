e8ecbaa2b5739ceb11467eb9b22487ff2c55e98d
Linlin Yan <linlin.yan@bioinfo.app>
2024-07-14 02:03:51 +0800

    Replace icon with new logo.

--- logo/convert-svg-to-png.sh
+++ logo/convert-svg-to-png.sh
@@ -1,4 +1,8 @@
-rsvg-convert -a paper-hub-logo.40x40.svg -o paper-hub-logo.40x40.png
+rsvg-convert -a -w 256 -h 256 paper-hub-logo.40x40.svg -o paper-hub-logo.256x256.png
+convert paper-hub-logo.256x256.png -define icon:auto-resize=256,128,64,48,32,16 paper-hub.ico
 
-rsvg-convert -a paper-hub-banner.40x150-white-text.svg -o paper-hub-banner.40x150-white-text.png
-rsvg-convert -a paper-hub-banner.40x150-black-text.svg -o paper-hub-banner.40x150-black-text.png
+rsvg-convert -a -w 300 -h 80 paper-hub-banner.150x40-white-text.svg -o paper-hub-banner.300x80-white-text.png
+rsvg-convert -a -w 300 -h 80  paper-hub-banner.150x40-black-text.svg -o paper-hub-banner.300x80-black-text.png
+
+cp -av paper-hub-banner.300x80-white-text.png ../static/images/banner-w.png
+cp -av paper-hub.ico ../static/favicon.ico
