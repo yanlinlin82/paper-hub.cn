9371d9b63ac8bcd32d4bc46131b738a5c2188078
Linlin Yan <linlin.yan@bioinfo.app>
2024-07-15 19:59:49 +0800

    Support searching.

--- group/views.py
+++ group/views.py
@@ -1,10 +1,24 @@
+from functools import wraps
+from urllib.parse import unquote
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count, F, Min, Q
 from core.models import UserProfile, GroupProfile
 from core.paper import get_this_week_start_time, get_check_in_interval, get_this_month, get_stat_all, get_stat_this_month, get_stat_last_month, get_stat_journal, get_last_month
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Count, F, Min
-from urllib.parse import unquote
+
+def redirect_query(func):
+    @wraps(func)
+    def wrapper(request, group_name, *args, **kwargs):
+        query = request.GET.get('q', '').strip()
+        if query != '':
+            url = reverse('group:all', args=[group_name])
+            full_url = f"{url}?{request.GET.urlencode()}"
+            return redirect(full_url)
+        request.query = query
+        return func(request, group_name, *args, **kwargs)
+    return wrapper
 
 def get_paginated_reviews(reviews, page_number):
     if page_number is None:
@@ -25,10 +39,28 @@ def get_paginated_reviews(reviews, page_number):
 
     return reviews, zip(items, indices)
 
+def filter_review_by_query(reviews, query):
+    if query != '':
+        print(f"query: {query}")
+        print("before filter:", reviews.count())
+        reviews = reviews.filter(
+            Q(creator__nickname=query) |
+            Q(comment__icontains=query) |
+            Q(paper__title__icontains=query) |
+            Q(paper__authors__icontains=query) |
+            Q(paper__journal__icontains=query) |
+            Q(paper__abstract__icontains=query) |
+            Q(paper__keywords__icontains=query))
+        print("after filter:", reviews.count())
+    return reviews
+
 def my_sharing_page(request, group_name):
     group = get_object_or_404(GroupProfile, name=group_name)
     user = UserProfile.objects.get(auth_user=request.user)
-    reviews = group.reviews.filter(creator=user).order_by('-create_time', '-pk')
+    reviews = group.reviews.filter(creator=user, delete_time__isnull=True)
+    query = request.GET.get('q', '').strip()
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-create_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -41,10 +73,12 @@ def my_sharing_page(request, group_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_my_sharing',
+        'query': query,
         'reviews': reviews,
         'items': items,
     })
 
+@redirect_query
 def index_page(request, group_name):
     group = get_object_or_404(GroupProfile, name=group_name)
 
@@ -55,7 +89,10 @@ def index_page(request, group_name):
 
 def all_page(request, group_name):
     group = get_object_or_404(GroupProfile, name=group_name)
-    reviews = group.reviews.filter(delete_time__isnull=True).order_by('-create_time', '-pk')
+    reviews = group.reviews.filter(delete_time__isnull=True)
+    query = request.GET.get('q', '').strip()
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-create_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -68,6 +105,7 @@ def all_page(request, group_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_all',
+        'query': query,
         'reviews': reviews,
         'items': items,
     })
@@ -75,7 +113,10 @@ def all_page(request, group_name):
 def recent_page(request, group_name):
     group = get_object_or_404(GroupProfile, name=group_name)
     start_time = get_this_week_start_time()
-    reviews = group.reviews.filter(create_time__gte=start_time).order_by('-create_time', '-pk')
+    reviews = group.reviews.filter(create_time__gte=start_time, delete_time__isnull=True)
+    query = request.GET.get('q', '').strip()
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-create_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -88,6 +129,7 @@ def recent_page(request, group_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_recent',
+        'query': query,
         'reviews': reviews,
         'items': items,
     })
@@ -95,7 +137,10 @@ def recent_page(request, group_name):
 def this_month_page(request, group_name):
     group = get_object_or_404(GroupProfile, name=group_name)
     start_time, end_time = get_check_in_interval(*get_this_month())
-    reviews = group.reviews.filter(create_time__gte=start_time).order_by('-create_time', '-pk')
+    reviews = group.reviews.filter(create_time__gte=start_time, delete_time__isnull=True)
+    query = request.GET.get('q', '').strip()
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-create_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -108,6 +153,7 @@ def this_month_page(request, group_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_this_month',
+        'query': query,
         'reviews': reviews,
         'items': items,
     })
@@ -115,7 +161,10 @@ def this_month_page(request, group_name):
 def last_month_page(request, group_name):
     group = get_object_or_404(GroupProfile, name=group_name)
     start_time, end_time = get_check_in_interval(*get_last_month(*get_this_month()))
-    reviews = group.reviews.filter(create_time__gte=start_time, create_time__lt=end_time).order_by('-create_time', '-pk')
+    reviews = group.reviews.filter(create_time__gte=start_time, create_time__lt=end_time, delete_time__isnull=True)
+    query = request.GET.get('q', '').strip()
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-create_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -128,6 +177,7 @@ def last_month_page(request, group_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_last_month',
+        'query': query,
         'reviews': reviews,
         'items': items,
     })
@@ -137,7 +187,10 @@ def trash_page(request, group_name):
         return redirect('/')
 
     group = get_object_or_404(GroupProfile, name=group_name)
-    reviews = group.reviews.filter(delete_time__isnull=False).order_by('-delete_time', '-pk')
+    reviews = group.reviews.filter(delete_time__isnull=False)
+    query = request.GET.get('q', '').strip()
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-delete_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -150,10 +203,12 @@ def trash_page(request, group_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_trash',
+        'query': query,
         'reviews': reviews,
         'items': items,
     })
 
+@redirect_query
 def single_page(request, id, group_name):
     group = get_object_or_404(GroupProfile, name=group_name)
     review = get_object_or_404(group.reviews, pk=id)
@@ -171,7 +226,10 @@ def single_page(request, id, group_name):
 def journal_page(request, group_name, journal_name):
     journal_name = unquote(journal_name)
     group = get_object_or_404(GroupProfile, name=group_name)
-    reviews = group.reviews.filter(paper__journal=journal_name)
+    reviews = group.reviews.filter(paper__journal=journal_name, delete_time__isnull=True)
+    query = request.GET.get('q', '').strip()
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-create_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -184,6 +242,7 @@ def journal_page(request, group_name, journal_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_journal',
+        'query': query,
         'journal_name': journal_name,
         'reviews': reviews,
         'items': items,
@@ -192,7 +251,11 @@ def journal_page(request, group_name, journal_name):
 def user_page(request, id, group_name):
     user = get_object_or_404(UserProfile, pk=id)
     group = get_object_or_404(GroupProfile, name=group_name)
-    reviews = group.reviews.filter(creator=user).order_by('-create_time', '-pk')
+    reviews = group.reviews.filter(creator=user, delete_time__isnull=True)
+    query = request.GET.get('q', '').strip()
+    print(f"query: {query}")
+    reviews = filter_review_by_query(reviews, query)
+    reviews = reviews.order_by('-create_time', '-pk')
     page_number = request.GET.get('page')
     reviews, items = get_paginated_reviews(reviews, page_number)
 
@@ -205,6 +268,7 @@ def user_page(request, id, group_name):
     return render(request, 'group/list.html', {
         'group': group,
         'current_page': 'group_user',
+        'query': query,
         'user_info': user,
         'reviews': reviews,
         'items': items,
@@ -273,8 +337,10 @@ def _rank_page(request, group_name, rank_type):
         'month_list': [i for i in range(1, 13)],
     })
 
+@redirect_query
 def rank_page(request, group_name):
     return _rank_page(request, group_name, 'this_month')
 
+@redirect_query
 def rank_type_page(request, group_name, rank_type):
     return _rank_page(request, group_name, rank_type)
