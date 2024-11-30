8d61ea02b73e662200ad5337421b2ac2bcceb190
Linlin Yan <linlin.yan@bioinfo.app>
2024-11-30 09:38:35 +0800

    Rename and update daily update script.

--- scripts/daily-update.sh
+++ scripts/daily-update.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+echo "========== $(date) =========="
+echo ">>> Updating for paper-hub.cn"
+
 BAK_FILE=db.sqlite3.bak-$(date +%Y%m%d)
 if [ -e "${BAK_FILE}" ]; then
-	echo "It has been updated already today"
+	echo ">>> It has been updated already today"
 	exit 0
 fi
 cp -av db.sqlite3 ${BAK_FILE}
 
 PUBMED_XML_GZ=$(find pubmed/updatefiles/ -type f -name '*.xml.gz' | sort | tail -n1)
-echo "Latest PubMed XML file: $PUBMED_XML_GZ"
+echo ">>> Latest PubMed XML file: $PUBMED_XML_GZ"
 
 SOURCE=$(basename $PUBMED_XML_GZ .xml.gz)
 SOURCE=${SOURCE#pubmed24n}
-echo "Got source: $SOURCE"
+echo ">>> Got source: $SOURCE"
 
 mkdir -pv log/pubmed
 ./.venv/bin/python scripts/import-pubmed.py pubmed $SOURCE -r -m default >>log/pubmed/$SOURCE.log 2>&1
 ./.venv/bin/python scripts/import-pubmed.py pubmed $SOURCE -r -m update-index >>log/pubmed/$SOURCE-update-index.log 2>&1
+
+echo ">>> Updating for paper-hub.cn done"
