9cd128d9306e21e6894babe4615cfaa1393da988
Linlin Yan <linlin.yan@bioinfo.app>
2025-09-12 02:21:22 +0800

    refactor: Remove jquery dependency

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,8 +114,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+# ref: https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
